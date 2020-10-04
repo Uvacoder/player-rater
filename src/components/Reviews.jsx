45dda82c69866380b8273d844b422ea507be684c
edwardz8 <tolklorist@gmail.com>
2020-10-03 21:23:40 -0400

    ready for deploy

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -4,10 +4,10 @@ import StarRating from './StarRating'
 const Reviews = ({ reviews }) => {
     
     return (
-        <div className="container row row-cols-3 mb-2 mt-4 d-flex">
+        <div className="container row row-cols-2 mb-2 mt-4 d-flex flex-wrap">
             {reviews.map((review) => {
                 return (
-           <div key={review.id} className="card bg-light mb-3 ml-2 mr-2" style={{maxWidth: "30%", minWidth: "30%"}}>
+           <div key={review.id} className="card bg-light mb-3 ml-2 mr-2">
                 <div className="card-header d-flex justify-content-between">
                     <span>{review.name}</span>
                     <span><StarRating rating={review.rating} /></span>
