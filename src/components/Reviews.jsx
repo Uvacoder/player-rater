3893d14a9e4c2cdd05d48bf4ea066f2bf29c30c0
edwardz8 <tolklorist@gmail.com>
2020-10-03 21:13:07 -0400

    ready for deploy

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -7,7 +7,7 @@ const Reviews = ({ reviews }) => {
         <div className="container row row-cols-3 mb-2 mt-4 d-flex">
             {reviews.map((review) => {
                 return (
-           <div key={review.id} className="card bg-light mb-3 ml-2 mr-2" style={{maxWidth: "30%"}}>
+           <div key={review.id} className="card bg-light mb-3 ml-2 mr-2" style={{maxWidth: "30%", minWidth: "30%"}}>
                 <div className="card-header d-flex justify-content-between">
                     <span>{review.name}</span>
                     <span><StarRating rating={review.rating} /></span>
